0f2eac80ba2d663db92bb6fb9fd84280b476a0fd
Thomas Vanderweeën <thomas.vanderweeen.w4430@student.hogent.be>
2018-02-27 16:04:41 +0100

    Implementatie ResourceBundle + taalselectie + vertaling Nl/Fr/En + verbeteringen bestaande code

--- src/cui/NewMain.java
+++ src/cui/NewMain.java
@@ -1,16 +1,11 @@
 package cui;
 
-import domein.DomeinController;
-import domein.Speler;
-import java.util.Scanner;
-
 /**
  *
  * @author Michiel S.
  */
 public class NewMain {
-        private final static DomeinController dc = new DomeinController();
-        private final Scanner sc = new Scanner(System.in);
+        
  /*main klasse voornamelijk gebruikt om te debuggen*/
  /*Hoe werkt Resources was leuk geweest als iemand die kon impelementeren na afspraak. ;) */
     public static void main(String[] args) {
--- src/cui/UC1.java
+++ src/cui/UC1.java
@@ -7,6 +7,7 @@
 package cui;
 import domein.DomeinController;
 import java.util.Scanner;
+import java.util.ResourceBundle;
 /**
  *
  * @author ThomasV
@@ -14,29 +15,73 @@ import java.util.Scanner;
 public class UC1 {
     private final DomeinController dc;
     private final Scanner sc = new Scanner(System.in);
+    private ResourceBundle r;
      /* Hier gaat de applicatie komen veronderstel ik*/
     public UC1(){
-        dc = new DomeinController();
+        this.dc = new DomeinController();
     }
     
     public void Start(){
+        taalSelectie();
+        toonMenu();
+    }
+    
+    private void taalSelectie(){
+        boolean taalGeselecteerd = false;
+        do {
+            try {
+                System.out.print("Welke taal wilt u?, Quelle langue vous préférez?, Which language do you prefer? (1:Nederlands, 2:English, 3:Français)");
+                int invoerTaal = sc.nextInt();
+
+                if (invoerTaal < 1 || invoerTaal > 3) {
+                    throw new IllegalArgumentException();
+                }
+
+                switch (invoerTaal) {
+                    case 1:
+                        r = ResourceBundle.getBundle("resources/Nederlands_ne");
+
+                        break;
+                    case 2:
+                        r = ResourceBundle.getBundle("resources/English_en");
+
+                        break;
+                    case 3:
+                        r = ResourceBundle.getBundle("resources/Français_fr");
+
+                        break;
+                }
+
+                taalGeselecteerd = true;
+                System.out.print(r.getString("taalKeuze"));
+                System.out.println();
+
+            } catch (IllegalArgumentException e) {
+                System.err.printf("Fout nummer, wrong number, numéro incorrect%n");
+            }
+        } while (!taalGeselecteerd);
+    }
+    
+    private void toonMenu(){
         int keuze = 0;
         
-        do{
-            System.out.printf("%s%n%s%n%s%n","Welkom tot de mastermind applicatie!","1)Meld aan" ,"2)Registreer");
-            keuze = geefKeuzeIn("Geef u keuze in: ");
-        }while(keuze<1||keuze>2);
+   
+            do{
+                System.out.printf("%s%n%s%n%s%n",r.getString("welkom"),r.getString("meldAan") ,r.getString("registreer"));
+                keuze = geefKeuzeIn(r.getString("keuzeInvoer"));
+            }while(keuze<1||keuze>2);
        
-        switch(keuze){
-            case 1:
-                meldAan();
-                System.out.println("Welkom terug "+dc.geefSpelerNaam());
-                break;
-            case 2:
-                registreer();
-                System.out.println("Welkom "+dc.geefSpelerNaam());
-                break;
-        }
+        
+            switch(keuze){
+                case 1:
+                    meldAan();
+                    System.out.println(r.getString("meldAanWelkom")+ " " + dc.geefSpelerNaam());
+                    break;
+                case 2:
+                    registreer();
+                    System.out.println(r.getString("registreerWelkom")+ " " +dc.geefSpelerNaam());
+                    break;
+            }
     }
     
     private int geefKeuzeIn(String boodschap){
@@ -45,10 +90,10 @@ public class UC1 {
     }
     
     private void meldAan(){
-        System.out.print("Geef uw spelernaam in: ");
+        System.out.print(r.getString("naamInvoer"));
         String naam = this.sc.next();
         
-        System.out.print("Geef uw wachtwoord in: ");
+        System.out.print(r.getString("wachtwoordInvoer"));
         String wachtwoord = this.sc.next();
         
         dc.meldAan(naam, wachtwoord);
@@ -56,13 +101,13 @@ public class UC1 {
     
         
     private void registreer(){
-        System.out.print("Geef een spelernaam in: ");
+        System.out.print(r.getString("registreerNaam"));
         String naam = this.sc.next();
         
-        System.out.print("Geef een wachtwoord in (bevat drie cijfers van voor, vervolgens zes karakters en drie cijfers achteraan): ");
+        System.out.print(r.getString("registreerWachtwoord"));
         String wachtwoord = this.sc.next();
         
-        System.out.print("Bevestig u wachtwoord: ");
+        System.out.print(r.getString("bevestigWachtwoord"));
         String wachtwoordBevestiging = this.sc.next();
         
         dc.registreer(naam, wachtwoord,wachtwoordBevestiging);
