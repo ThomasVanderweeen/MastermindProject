0dcae7b8059c2a617fd4bb1bba548df170341b47
FerreVerstichelen <ferre.verstichelen@student.hogent.be>
2018-03-27 09:23:57 +0200

    cui lijst spellen laden

--- src/cui/UC4.java
+++ src/cui/UC4.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cui;
 
 import domein.DomeinController;
+import java.util.InputMismatchException;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
@@ -16,7 +13,7 @@ import java.util.Scanner;
 public class UC4
 {
 
-    private ResourceBundle r;
+    private final ResourceBundle r;
     private final DomeinController dc;
     private final Scanner sc;
     private final UC3 uc3;
@@ -25,32 +22,56 @@ public class UC4
         this.r = r;
         this.dc = dc;
         this.sc = new Scanner(System.in);
-        this.uc3 = new UC3(dc,r);
+        this.uc3 = new UC3(dc, r);
     }
-    
-    /*private int geefNaamIn(){
-    }*/
-    
+
+    private int geefNaamIn() {
+        Scanner scan = new Scanner(System.in);
+        int keuze=0;
+        try {
+            System.out.printf("%n%s",r.getString("keuzeInTeLadenSpel"));
+            keuze = scan.nextInt();
+            
+        } catch (InputMismatchException i) {
+            System.out.printf("Je moet een geheel getal in geven,You have to enter a whole number,\n"
+                    + "votre choix doit être un nombre entier\n");
+            scan.next();
+        }
+        return keuze;
+    }
+
     /*start methode: -laadSpellen geef ze weer, geef naam in, laadSpelIn, als 
     er een spel ingeladen in + uc3 start methode aan roepen*/
-
+    public void start(){
+        laadSpellen();
+        int keuze = geefNaamIn();
+        laadSpelIn(keuze);
+    }
     private void laadSpellen() {
         String[][] spellen = this.dc.geefOpgeslagenSpellen();
-        String res = String.format("%10s %10s", r.getString("spelNaam"),r.getString("moeilijkheidsGraad"));
+        int index = 0;
+        String res = String.format("%15s %15s", r.getString("spelNaam"), r.getString("moeilijkheidsGraad"));
         for (String[] rij : spellen) {
-            res +=String.format("%n%10s", rij[0]);
-            switch(rij[1]){
-                case "1": res+=String.format("%10s",r.getString("makkelijk"));
+            index++;
+            res += String.format("%n%d) %15s", index, rij[0]);
+            switch (rij[1]) {
+                case "1":
+                    res += String.format("%15s", r.getString("makkelijk"));
                     break;
-                case "2": res+=String.format("%10s",r.getString("gemiddeld"));
+                case "2":
+                    res += String.format("%15s", r.getString("gemiddeld"));
                     break;
-                case "3": res+=String.format("%10s",r.getString("moeilijk"));
+                case "3":
+                    res += String.format("%15s", r.getString("moeilijk"));
                     break;
             }
-            
+
         }
         System.out.printf(res);
     }
-    
-    /*laadSpel In*/
+
+    public void laadSpelIn(keuze){
+        String[][] spellen = this.dc.geefOpgeslagenSpellen();
+        
+    }
 }
