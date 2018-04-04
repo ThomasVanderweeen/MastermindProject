94eb2b513e5ae33ba7f695f52382077c6719fc00
FerreVerstichelen <ferre.verstichelen@student.hogent.be>
2018-04-04 14:54:30 +0200

    cui lijst spellen laden

--- src/domein/Code.java
+++ src/domein/Code.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domein;
 import java.util.List;
-/**
- *
- * @author Groep 77
- */
+
 public class Code {
     private final List<CodePin> code;
     private MoeilijkheidsGraad moeilijkheidsGraad;
