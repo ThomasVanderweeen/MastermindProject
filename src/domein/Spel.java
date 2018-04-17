9e3514ec56f0bd35409ac6774dbcda57c190dee6
Thomas Vanderweeën <thomas.vanderweeen.w4430@student.hogent.be>
2018-04-17 08:54:09 +0200

    Javadoc voor elke methode

--- src/domein/Spel.java
+++ src/domein/Spel.java
@@ -11,22 +11,43 @@ public class Spel {
     private String naam;
     
     
+     /**
+     * Constructor voor een Spel object. Gebruikt controleerMoeilijkheidsGraad om de geldigheid van de moeilijkheidsgraad te verifiëren. 
+     * 
+     * @param moeilijkheidsGraad moeilijkheidsgraad van een spel
+     * @param speler speler van een spel
+     */
     public Spel(int moeilijkheidsGraad,Speler speler){
         this.speler = speler;
         controleerMoeilijkheidsGraad(moeilijkheidsGraad);
         this.spelbord = new Spelbord(moeilijkheidsGraad);
     }
     
+    /**
+     * Constructor voor een spel object.
+     * 
+     * @param spelbord spelbord van een spel
+     * @param speler speler van een spel
+     */
     public Spel(Spelbord spelbord,Speler speler){
         this.speler = speler;
         this.spelbord = spelbord;
     }
     
-    
+    /**
+     * getAantalPogingen geeft het aanralPogingen van een spel object.
+     * 
+     * @return int
+     */
     public int getAantalPogingen(){
         return this.aantalPogingen;
     }
     
+    /**
+     * controleerMoeilijkheidsGraad controleert 
+     * 
+     * @param moeilijkheidsGraad de moeilijkheidsgraad die gecontroleerd wordt. 
+     */
     private void controleerMoeilijkheidsGraad(int moeilijkheidsGraad){
        if(moeilijkheidsGraad==2){
            if(this.speler.getAantalGewonnenMakkelijk()<20)
@@ -39,14 +60,29 @@ public class Spel {
        }
     }
     
+    /**
+     * getSpelBord geeft het spelbord van een spel object.
+     * 
+     * @return Spelbord
+     */
     public Spelbord getSpelBord(){
         return this.spelbord;
     }
     
+    /**
+     * isGewonnen bepaalt of de speler gewonnen is.
+     * 
+     * @return boolean
+     */
     public boolean isGewonnen(){
         return this.spelbord.getCodeGeraden();
     }
     
+    /**
+     * bepaalEindeSpel bepaalt of het spel ten einde is.
+     * 
+     * @return bolean. 
+     */
     public boolean bepaalEindeSpel(){
         if ((getAantalPogingen() >= 12)||isGewonnen())
             return true;
@@ -54,21 +90,40 @@ public class Spel {
         return false;
     }
     
+    /**
+     * doePoging voegt een poging toe aan het spelbord.
+     * 
+     * @param poging poging die wordt toegevoegd
+     */
     public void doePoging(int[] poging){
         int rij = getAantalPogingen();
         this.spelbord.voegPogingToe(poging, rij);
         this.aantalPogingen ++;
     }
     
+    /**
+     * geefMoeilijkheidsGraad geeft de moeilijkheidsgraad van een spel object.
+     * 
+     * @return int 
+     */
     public int geefMoeilijkheidsGraad(){
         return this.spelbord.geefMoeilijkheidsGraad();
     }
     
+    /**
+     * getSpeler geeft de speler van een spel object.
+     * 
+     * @return Speler 
+     */
     public Speler getSpeler(){
         return this.speler;
     }
     
-
+    /**
+     * geefAantalSterrenenAantalTotVolgende geeft het aantal sterren dat een speler reeds heeft behaald en hoeveel sterren deze speler nog nodig heeft om de volgende moeilijkheidsgraad te ontgrendelen.
+     * 
+     * @return int[]
+     */
     public int[] geefAantalSterrenEnAantalTotVolgende(){
         
         int moeilijkheidsGraad = this.spelbord.geefMoeilijkheidsGraad();
@@ -76,10 +131,20 @@ public class Spel {
 
     }
     
+    /**
+     * getNaam geeft de naam van een spelObject.
+     * 
+     * @return String 
+     */
     public String getNaam(){
         return this.naam;
     }
     
+    /**
+     * stelNaamIn verandert de naam van een spel object.
+     * 
+     * @param naam de naam van het spel
+     */
     public void stelNaamIn(String naam){
         this.naam = naam;
     }
--- src/domein/SpelRepository.java
+++ src/domein/SpelRepository.java
@@ -12,11 +12,19 @@ public class SpelRepository {
     private List<Spel> spelletjes;
     private final SpelMapper spm;
     
+    /**
+     * constructor voor een spelRepository object.
+     */
     public SpelRepository(){
         spelletjes = new ArrayList<>();
         this.spm = new SpelMapper(); 
     }
     
+    /**
+     * voegSpelToe voegt een spel toe aan de databank via de spelmapper.
+     * 
+     * @param spel spel dat aan de databank wordt toegevoegd.
+     */
     public void voegSpelToe(Spel spel){
         spelletjes.add(spel);
         try{
@@ -26,13 +34,24 @@ public class SpelRepository {
         }
     }
    
+    /**
+     * controleerNaam controleert of de naam van een spel uniek is.
+     * 
+     * @param naam naam die geconrtoleerd wordt.
+     * @return 
+     */
     public boolean controleerNaam(String naam){
         if(this.spm.spelBestaat(naam))
             throw new SpelNaamNietUniekException();
         else
             return false;
     }
-
+    /**
+     * toonSpellen haalt een lijst van spellen uit de databank van een bepaalde speler.
+     * 
+     * @param spelernaam speler waarvan de spellen uit de databank wordt gehaald.
+     * @return String[][]
+     */
     public String[][] toonSpellen(String spelernaam){
         if(this.spm.heeftOpgeslagenSpellen(spelernaam))
             return this.spm.toonSpellen(spelernaam);
@@ -40,10 +59,22 @@ public class SpelRepository {
             throw new SpelerHeeftGeenOpgeslagenSpellenException();
     }
     
+    /**
+     * laadSpel laadt een spel uit de databank.
+     * 
+     * @param spelnaam naam van het spel dat wordt geladen.
+     * @param speler naam van de speler van wie het spel is.
+     * @return Spel
+     */
     public Spel laadSpel(String spelnaam,Speler speler){
         return this.spm.maakSpel(spelnaam, speler);
     }
     
+    /**
+     * verwijderSpel verwijdert een spel uit de databank
+     * 
+     * @param spelnaam naam van het spel dat uit de databank verwijdert wordt.
+     */
     public void verwijderSpel(String spelnaam){
         this.spm.verwijderSpel(spelnaam);
     }
