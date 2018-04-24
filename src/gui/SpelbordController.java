e2d539b936fb09e6b4fe39ee1906f4e4587b06e7
FerreVerstichelen <ferre.verstichelen@student.hogent.be>
2018-04-24 11:07:44 +0200

    Scene builder achtergronden toegevoegd en lay out verbeterd

---
+++ src/gui/SpelbordController.java
@@ -0,0 +1,42 @@
+
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class SpelbordController implements Initializable
+{
+
+    @FXML
+    private Button engels;
+    @FXML
+    private Button frans;
+    @FXML
+    private ImageView nederlands;
+    @FXML
+    private Label logInLabel;
+    @FXML
+    private Button startSpel;
+    @FXML
+    private Button laadSpel;
+    @FXML
+    private Button daagUit;
+    @FXML
+    private Button bekijkUitdagingen;
+    @FXML
+    private Button bekijkScorebord;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
