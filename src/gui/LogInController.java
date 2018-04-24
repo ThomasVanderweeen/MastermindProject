17082dc231e003bf6f15fea45ff277f2d031a787
Thomas Vanderweeën <thomas.vanderweeen.w4430@student.hogent.be>
2018-04-24 11:22:48 +0200

    LogInController overgang naar Menuscherm

--- src/gui/LogInController.java
+++ src/gui/LogInController.java
@@ -12,11 +12,13 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,7 +29,7 @@ public class LogInController implements Initializable
 {
 
     private ResourceBundle r;
-    private ScreenController sc;
+    private ScreenController sc = new ScreenController();
     private final MenuScherm ms = new MenuScherm();
     
     @FXML
@@ -71,7 +73,14 @@ public class LogInController implements Initializable
                 logInWachtwoord.getText());
         }
         catch(AanmeldException e){
-            foutmelding.setText("Combinatie gebruikersnaam en wachtwoord niet bekend in het systeem");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Combinatie wachtwoord en speler");
+            alert.setHeaderText("Kan speler niet inloggen");
+            alert.setContentText("combinatie speler en wachtwoord niet gekend in het systeem");
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            stage.setAlwaysOnTop(true);
+            stage.toFront();
+            stage.show();
             return;
         }
         logInNaam.clear();
